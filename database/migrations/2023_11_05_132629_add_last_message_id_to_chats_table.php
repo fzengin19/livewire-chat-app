63b85a57448d8f0829f2fbf797e2b0ffb42e0f20
fatih zengin <fatihzengin654@outlook.com>
2023-11-16 02:22:34 +0300

    last_message_id column in chat table changed to null

--- database/migrations/2023_11_05_132629_add_last_message_id_to_chats_table.php
+++ database/migrations/2023_11_05_132629_add_last_message_id_to_chats_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('chats', function (Blueprint $table) {
-            $table->foreignId('last_message_id')->references('id')->on('messages');
+            $table->foreignId('last_message_id')->nullable()->references('id')->on('messages');
         });
     }
 
